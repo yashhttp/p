3774c8d041643aedafae658b11ecb98cf96adb05
YASH SAINI <yashsainishamli12@gmail.com>
2026-04-29 14:53:07 +0530

    create document services

---
+++ Backend/src/modules/document/document.service.js
@@ -0,0 +1,17 @@
+import {Document} from "./document.model.js";
+
+export const uploadDocument = async(userId, file, type)=>{
+    const doc = await Document.create({
+        user : userId,
+        fileName : file.filename,
+        originalName : file.originalname,
+        mimeType : file.mimetype,
+        size : file,size,
+        url : `/uploads/${file.filename}`,
+        type,
+
+
+    })
+    return doc;
+    
+}

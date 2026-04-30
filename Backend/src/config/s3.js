35560cf1067698d01238686526bb1031e0eb1eec
YASH SAINI <yashsainishamli12@gmail.com>
2026-04-30 19:04:30 +0530

    add files in ai modules

--- Backend/src/config/s3.js
+++ Backend/src/config/s3.js
@@ -1,20 +1,7 @@
-
 import dotenv from "dotenv";
 dotenv.config();
-
 import { S3Client } from "@aws-sdk/client-s3";
 
-
-// if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-//   throw new Error("❌ AWS credentials missing in .env");
-// }
-console.log({
-  key: process.env.AWS_ACCESS_KEY_ID,
-  keyLen: process.env.AWS_ACCESS_KEY_ID?.length,
-  secretLen: process.env.AWS_SECRET_ACCESS_KEY?.length,
-  region: process.env.AWS_REGION,
-  bucket: process.env.AWS_BUCKET_NAME,
-});
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
